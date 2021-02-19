c51e02e8be126e347564afbd68457f6f51621f90
KyanR <rowse9@gmail.com>
2021-02-19 17:13:53 +1100

    DingoFootstepsScript

---
+++ AshlandProject/Assets/Scripts/FMOD/F_Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+public class F_Player : MonoBehaviour
+{
+    [SerializeField]
+    private string footstepEvent;
+    [SerializeField]
+    private string materialParameter;
+    
+    public void DingoStep()
+    {
+        EventInstance dingoFootstep = RuntimeManager.CreateInstance(footstepEvent);
+        dingoFootstep.setParameterByName(materialParameter, 1);
+        dingoFootstep.start();
+        dingoFootstep.release();
+    }
+}
